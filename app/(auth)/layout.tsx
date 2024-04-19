bee522f865cab5c9e2afce84a2e6b54d83d89538
NAVI1322 <rksharma041976@gmail.com>
2024-04-19 14:54:07 -0400

    clerk and theme(darkAndLight Mode Added)

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,6 @@
+export default function RootLayout({ children }:{children :React.ReactNode}) {
+
+
+    return (  <div className="h-screen flex items-center justify-center">{children}</div>
+    );
+  }
